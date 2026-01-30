97f10558fefa28b517551b627f5b567c94759a5d
Vincent Chaussepied <vincent.chaussepied@gmail.com>
2026-01-30 15:31:53 +0000

    re-initalize projects

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "#151312",
+        ratingBox: "#221F3D",
+        searchBar: "#0F0D23",
+        text: "#9CA4AB",
+        darkAccent: "#AB8BFF",
+        accentText: "#A8B5DB",
+        secondaryText: "#D6C7FF",
+      },
+    },
+  },
+  plugins: [],
+};
