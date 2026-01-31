d493256cc79e8cb4ba18820bafc17ae8ee4c4bb9
Vincent Chaussepied <vincent.chaussepied@gmail.com>
2026-01-31 22:12:43 +0000

    Add initial implementation of movie fetching and search functionality
    - Create .env for API key configuration
    - Add Prettier configuration file
    - Update app.json with new app name and logo
    - Implement custom TabIcon component for tab navigation
    - Create SearchBar component for movie search
    - Add API service for fetching movies from TMDB
    - Implement useFetch hook for data fetching with error handling

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,28 +2,69 @@ import { icons } from '@/constants/icons'
 import { images } from '@/constants/images'
 import { Tabs } from 'expo-router'
 import React from 'react'
-import { Image, ImageBackground } from 'react-native'
+import { Image, ImageBackground, Text, View } from 'react-native'
 
 const _layout = () => {
+
+  const TabIcon = ({ icon, title, isFocused }: any) => {
+    if (isFocused) {
+      return (
+        <ImageBackground
+          source={images.highlight}
+          className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
+        >
+          <Image
+            source={icon}
+            tintColor='#151312'
+            className="size-5"
+            />
+            <Text className="text-secondary text-base font-semibold ml-2">{title}</Text>
+        </ImageBackground>
+      )
+    }
+    return (
+      <View className="size-full justify-center items-center mt-4 rounded-full">
+        <Image source={icon} tintColor="#A8B5db" className="size-5" />
+      </View>
+
+    )
+  }
+
+
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle:{
+          width: "100%",
+          height: "100%",
+          justifyContent:"center",
+          alignItems: "center"
+        },
+        tabBarStyle:{
+          backgroundColor: "#0f0d23",
+          borderRadius: 50,
+          marginHorizontal: 20,
+          marginBottom: 36,
+          height: 52,
+          position: 'absolute',
+          overflow: "hidden",
+          borderWidth: 1,
+          borderColor: "#0f0d23"
+        }
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
           headerShown: false,
           tabBarIcon: ({ focused }) => (
-          <>
-              <ImageBackground
-                source={images.highlight}
-              >
-                <Image
-                  source={icons.home}
-                  tintColor='#151312'
-                  className="size-5"
-                  />
-              </ImageBackground>
-            </>
+            <TabIcon
+              icon={icons.home}
+              title="Home"
+              isFocused={focused}
+            />
           )
         }}
       />
@@ -32,20 +73,43 @@ const _layout = () => {
         options={{
           headerShown: false,
           title: 'Search',
+          tabBarIcon: ({ focused }) => (
+            <TabIcon
+              icon={icons.search}
+              title="Search"
+              isFocused={focused}
+            />
+          )
         }}
       />
+
       <Tabs.Screen
         name="saved"
         options={{
           headerShown: false,
           title: 'Saved',
+          tabBarIcon: ({ focused }) => (
+            <TabIcon
+              icon={icons.save}
+              title="Saved"
+              isFocused={focused}
+            />
+          )
         }}
       />
+
       <Tabs.Screen
         name="profile"
         options={{
           headerShown: false,
           title: 'Profile',
+          tabBarIcon: ({ focused }) => (
+            <TabIcon
+              icon={icons.person}
+              title="Profile"
+              isFocused={focused}
+            />
+          )
         }}
       />
     </Tabs>
---
+++ components/services/useFetch.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+
+const useFetch = <T>(fetchFunction: () => Promise<T>, autoFetch = true) => {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+
+      const result = await fetchFunction();
+
+      setData(result);
+    } catch (err) {
+      setError(err instanceof Error ? err : new Error('An error has occured'));
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const reset = () => {
+    setData(null);
+    setLoading(false);
+    setError(null);
+  };
+
+  useEffect(() => {
+    if (autoFetch) {
+      fetchData();
+    }
+  }, []);
+
+  return { data, loading, error, reset, refetch: fetchData };
+};
+
+export default useFetch;
